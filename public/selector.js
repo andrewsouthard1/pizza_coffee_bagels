5d1f0372956d4b304120a405d74fd1b45e568cae
Andrew Southard <andrewsouthard111@gmail.com>
2016-08-30 05:35:49 -0400

    added taxi form style to all forms

--- public/selector.js
+++ public/selector.js
@@ -2,35 +2,36 @@
 $("#pizzaForm").hide();
 $("#coffeeForm").hide();
 $("#bagelForm").hide();
-
+$("#backLink").hide();
 
 $("#pizzaPic").on("click", function(){
 	$("#bagelPic").remove();
 	$("#coffeePic").remove();
-	$("#pizzaForm").fadeIn(1000);
 	$("#coffeeForm").remove();
 	$("#bagelForm").remove();
 	$(this).removeClass('col-md-4 col-sm-4');
+	$("#pizzaForm").fadeIn(1000);
+	$("#backLink").fadeIn(1000);
 	
 });
-
 
 $("#coffeePic").on("click", function(){
 	$("#pizzaPic").remove();
 	$("#bagelPic").remove();
-	$("#coffeeForm").fadeIn(1000);
-	$(this).removeClass('col-md-4 col-sm-4');
 	$("#pizzaForm").remove();
 	$("#bagelForm").remove();
-	// $(this).switchClass('col-md-4 col-sm-4 container row', 'text-center', 1000, 'easeInOutQuad')
+	$(this).removeClass('col-md-4 col-sm-4');
+	$("#coffeeForm").fadeIn(1000);
+	$("#backLink").fadeIn(1000);
 });
 
 $("#bagelPic").on("click", function(){
 	$("#pizzaPic").remove();
 	$("#coffeePic").remove();
-	$("#bagelForm").fadeIn(1000);
-	$(this).removeClass('col-md-4 col-sm-4');
 	$("#pizzaForm").remove();
 	$("#coffeeForm").remove();
+	$(this).removeClass('col-md-4 col-sm-4');
+	$("#coffeeForm").fadeIn(1000);
+	$("#backLink").fadeIn(1000);
 
 });
